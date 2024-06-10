3e1b56aa9709eeb5dea1016cf6e8ba08202aeb41
Adam DJ Brett <adamdjbrett@users.noreply.github.com>
2024-06-09 19:44:07 -0500

    Update get_rss.sh
    update rss link

--- get_rss.sh
+++ get_rss.sh
@@ -1,4 +1,4 @@
-wget -O feed.xml 'https://input.relcfp.com/p/i/?a=rss&rid=6304038b62d7d&user=wyattphd-admin&token=b5142b8a9c4208e145af4be2c86d4ad4&hours=168'
+wget -O feed.xml 'https://input.relcfp.com/feed.xml'
 sed -i 's/<!\[CDATA\[//g' feed.xml
 sed -i 's/]]>//g' feed.xml
 sed -i 's/<br>/<br\/>/g' feed.xml
